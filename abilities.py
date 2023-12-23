db34735659f5d88b4e4fa7d1c5c655a9a51391d9
Nathaniel Tjandra <ntjandra4973@gmail.com>
2023-12-22 18:37:10 -0800

    New Level Scaling (#8)
    * Add balance changes.
    
    * Redid level scales, no limit with recursion.
    
    * Rename function name.
    
    * Add memoization, rename functions.
    
    * Fix transformation bug.
    
    * Clean up whitespace.

--- abilities.py
+++ abilities.py
@@ -78,8 +78,9 @@ def apply(character, skill):
     return log
 
 
-def level_scale(level):
+def level_old_scale(level):
     """
+    Old level scale kept for special events.
     Inputs character level and outputs level scaling
     Level 0: 1
     Levels 1-10: Adds 0.2 for each level past previous statement
@@ -91,15 +92,49 @@ def level_scale(level):
     """
     depth = level / 10
     if depth > 4:
-        return 1 + (0.2 * 10) + (0.25 * (level - 10)) + (0.3 * (level - 20)) + (0.35 * (level - 30)) + (0.4 * (level - 40))
+        return 1 + (0.2 * level) + (0.25 * (level - 10)) + (0.3 * (level - 20)) + (0.35 * (level - 30)) + (0.4 * (level - 40))
     elif depth > 3:
-        return 1 + (0.2 * 10) + (0.25 * (level - 10)) + (0.3 * (level - 20)) + (0.35 * (level - 30))
+        return 1 + (0.2 * level) + (0.25 * (level - 10)) + (0.3 * (level - 20)) + (0.35 * (level - 30))
     elif depth > 2:
-        return 1 + (0.2 * 10) + (0.25 * (level - 10)) + (0.3 * (level - 20))
+        return 1 + (0.2 * level) + (0.25 * (level - 10)) + (0.3 * (level - 20))
     elif depth > 1:
-        return 1 + (0.2 * 10) + (0.25 * (level - 10))
+        return 1 + (0.2 * level) + (0.25 * (level - 10))
     else:
         return 1 + (0.2 * level)
+
+
+def level_scale(level, memo={}):
+    """
+    Level scale done recusively to apply for all values. This scales differently from the prior!
+    Pattern: Starts at a base of 0.2, then for every 10 levels increases the multiplier by 0.05.
+    This adds only the max of previous layers instead of the difference since reaching the layer.
+    Meant to ease the scaling rate of growth by capping multiplier values at 10.
+
+    The old leveling resulted in crazy growth "for each level" after 20, which broke balance.
+    Before: Level 21: 1 + (0.2 * 21) + (0.25 * (21 - 10)) + (0.3 * (21 - 20)) = 6.05
+    After: Level 21: 1 + (0.2 * 10) + (0.25 * 10) + (0.3 * (1)) = 5.8
+
+    Optimization: Added Memoization to avoid duplicate recalculations.
+    Limits: Handles up to Level 9970, due to python recusive depth limits.
+    """
+    if (level in memo):
+        return memo[level]
+
+    depth = (level-1) // 10  # Subtract 1 so digits [0-9] are a layer.
+
+    digit = level % 10
+    # Treat zero as ten
+    if digit == 0:
+        digit = 10
+    modifier = round(0.2 + (0.05 * depth), 2)
+
+    if level <= 10:
+        res = 1 + (modifier * digit)
+    else:
+        # Fetch remainder for the top layer, and repeat from peak of prior layers.
+        res = (digit * modifier) + level_scale(depth * 10)
+    memo[level] = res
+    return res
 
 
 def transform(character):
@@ -116,6 +151,7 @@ def transform(character):
         else:
             player["spell_resist"] = characters["shai_human"]["spell_resist"]
             player["weapon_power"] = characters["shai_human"]["weapon_power"]
+            player["is_transformed"] = False
             return "Shai'Rei returned to human form."
 
 
